c25882d60bdb57c0402906e1b5804ff87d21151e
K. Kartari <kkartari7@gmail.com>
2018-01-26 03:51:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "Let's talk about space";
+
+var conversation = `${greetSpecialGuest} ${topic}`;
